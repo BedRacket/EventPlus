643329a2900e167cf7cf4ff93bcfad16e7a9ba73
1798643961 <1798643961@qq.com>
2022-12-10 14:59:44 +0800

    Update icon add PlayerFishingEvent

---
+++ bedracket_entity_events/src/main/java/org/bedracket/entity_events/mixin/MixinFishingBobberEntity.java
@@ -0,0 +1,29 @@
+package org.bedracket.entity_events.mixin;
+
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.item.ItemStack;
+import org.bedracket.entity_events.event.player.PlayerFishingEvent;
+import org.bedracket.eventbus.event.BedRacket;
+import org.bedracket.eventbus.event.EventInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@EventInfo(events = "PlayerFishingEvent")
+@Mixin(FishingBobberEntity.class)
+public abstract class MixinFishingBobberEntity {
+
+    @Inject(method = "use",at=@At("HEAD"),cancellable = true)
+    private void callPlayerFishingEvent(ItemStack usedItem, CallbackInfoReturnable<Integer> cir) {
+        assert  ((FishingBobberEntity) (Object) this).getPlayerOwner() != null;
+        PlayerFishingEvent bedracketEvent =(PlayerFishingEvent) BedRacket.EVENT_BUS.post(
+                PlayerFishingEvent.class, new PlayerFishingEvent(
+                        ((FishingBobberEntity) (Object) this).getPlayerOwner(),
+                        ((FishingBobberEntity) (Object) this).getPlayerOwner().getWorld())
+        );
+        if (bedracketEvent.isCancelled()) {
+            cir.cancel();
+        }
+    }
+}
